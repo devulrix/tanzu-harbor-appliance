7b672d0ddad5dc0e4e2040a5cabc3902efde3e61
Ulrich <12871773+devulrix@users.noreply.github.com>
2022-03-10 11:38:02 +0100

    downloading tkr

--- scripts/photon-packages.sh
+++ scripts/photon-packages.sh
@@ -26,3 +26,15 @@ for row in $(jq -c '.bundles | map(.) | .[]' ${APPLIANCE_BOM_FILE}); do
     echo $(_jq '.source')
     imgpkg copy -b $(_jq '.source') --to-tar images/$(_jq '.source' | awk -F '/' '{print $NF}').tar
 done
+
+echo ' > Downloading tkr...'
+
+for row in $(jq -c '.tkr | map(.) | .[]' ${APPLIANCE_BOM_FILE}); do
+    _jq() {
+        echo ${row} | jq -r "${1}"
+    }
+
+    OVA=https://wp-content.vmware.com/v2/latest/$(curl -s https://wp-content.vmware.com/v2/latest/items.json | jq -cr --arg TKR "$(_jq '.name')" '.items[].files[].hrefs[0] | select (. | contains($TKR) ) | select(. | endswith("vmdk"))')
+    echo ${OVA}
+    wget -O images/$(_jq '.name').ova ${OVA}
+done
