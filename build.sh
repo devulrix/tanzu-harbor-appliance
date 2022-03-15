5296b828bbc73ef03a3d43c0c2a9f7b6b62a93d5
Ulrich <12871773+devulrix@users.noreply.github.com>
2022-03-15 12:49:44 +0100

    added local build support
    Tested with VMware Fusion

--- build.sh
+++ build.sh
@@ -4,15 +4,30 @@ set -euo pipefail
 
 APPLIANCE_BOM_FILE=tanzu-harbor-bom.json
 
+build_local=
+
+while getopts l flag
+do
+    case "${flag}" in
+        l) build_local=TRUE;;
+    esac
+done 
+
 if [! has jq 2>/dev/null]; then
   echo "jq is missing"
   exit 1
 fi
 
-echo "Building Harbor OVA Appliance ..."
+echo "Cleaning up old Harbor OVA Appliance ..."
 rm -f output-harbor-iso/*.ova
 
 APPLIANCE_VERSION=$(jq -r < ${APPLIANCE_BOM_FILE} '.appliance.version')
 
-echo "Applying packer build to photon.json ..."
-packer build -var "APPLIANCE_VERSION=${APPLIANCE_VERSION}"  -var-file=photon-builder.json -var-file=photon-version.json photon.json
+if [ -z "$build_local" ]
+then
+  echo "Applying packer build to photon.json ..."
+  packer build -var "APPLIANCE_VERSION=${APPLIANCE_VERSION}"  -var-file=photon-builder.json -var-file=photon-version.json photon.json
+else
+  echo "Applying packer build to photon.json ..."
+  packer build -var "APPLIANCE_VERSION=${APPLIANCE_VERSION}"  -var-file=photon-version.json photon-local.json
+fi
