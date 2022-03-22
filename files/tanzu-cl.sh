d13b66ceee635d89b05c7da969ee4ddfe4d076ac
Ulrich <12871773+devulrix@users.noreply.github.com>
2022-03-22 09:48:34 +0100

    tanzu cl post deploy
    Optionally create a content library and copy the TKR releases into the library

---
+++ files/tanzu-cl.sh
@@ -0,0 +1,46 @@
+#/bin/bash
+
+set -euo pipefail
+
+
+ usage() {
+     echo "Usage: $0 [ -v VCENTER URL ] [-u USER ] [-p PASSWORD ] [-l CONTENT_LIBRARY_NAME ] [-c CREATE CONTENT_LIBRARY ] [-d CONTENT_LIBRARY DATASTORE]"
+ }
+
+ exit_abnormal() {
+     usage
+     exit 1
+ }
+
+while getopts v:u:p:l:c flag
+do
+    case "${flag}" in
+        v) VCENTER_URL=${OPTARG};;
+        u) VCENTER_USER=${OPTARG};;
+        p) VCENTER_PASSWORD=${OPTARG};;
+        l) CONTENT_LIBRARY=${OPTARG};;
+        d) DATASTORE=${OPTARG};;
+        c) CREATE_CL=TRUE;;
+        *) exit_abnormal;;
+    esac
+done 
+
+export GOVC_INSECURE=1
+GOVC_USERNAME=${VCENTER_USER}
+GOVC_PASSWORD=${VCENTER_PASSWORD}
+
+export GOVC_URL="${GOVC_USERNAME}:${GOVC_PASSWORD}@${VCENTER_URL}"
+
+#create content library 
+if [[ ! -z CREATE_CL ]] then 
+    govc library.create -d "Tanzu Kubernetes Grid Releases" -ds ${DATASTORE} ${CONTENT_LIBRARY_NAME}
+fi 
+
+#import tkr 
+for row in $(jq -c '.tkr | map(.) | .[]' ${APPLIANCE_BOM_FILE}); do
+    _jq() {
+        echo ${row} | jq -r "${1}"
+    }
+
+    govc library.import ${CONTENT_LIBRARY} /root/images/$(_jq '.name').ova
+done
